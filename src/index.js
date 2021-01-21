e896ee5ccd71c1072fee50bf1effe8fddf01bcd2
yesimnoh <yesimnoh@gmail.com>
2021-01-22 00:01:11 +0900

    Make To-Do list with Vanilla-Redux

--- src/index.js
+++ src/index.js
@@ -1,37 +1,104 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+/*************** Counter Vanilla*/
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.querySelector("span");
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+// const ADD = "ADD";
+// const MINUS = "MINUS";
 
-const countModifier = (count = 0, action) => {
+// const countModifier = (count = 0, action) => {
+// 	switch (action.type) {
+// 		case ADD:
+// 			return count + 1;
+// 		case MINUS:
+// 			return count - 1;
+// 		default:
+// 			return count;
+// 	}
+// };
+
+// const countStore = createStore(countModifier);
+
+// const onChange = () => {
+// 	number.innerText = countStore.getState();
+// };
+// countStore.subscribe(onChange);
+
+// const handleAdd = () => {
+// 	countStore.dispatch({ type: ADD });
+// };
+
+// const handleMinus = () => {
+// 	countStore.dispatch({ type: MINUS });
+// };
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+/*************** End Counter Vanilla*/
+
+/***************** TO DO Vanilla*/
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+	return { type: ADD_TODO, text };
+};
+
+const deleteToDo = (id) => {
+	return { type: DELETE_TODO, id };
+};
+
+const reducer = (state = [], action) => {
 	switch (action.type) {
-		case ADD:
-			return count + 1;
-		case MINUS:
-			return count - 1;
+		case ADD_TODO:
+			const newItem = { text: action.text, id: Date.now() };
+			return [newItem, ...state];
+		case DELETE_TODO:
+			return state.filter((todo) => todo.id !== action.id);
 		default:
-			return count;
+			return state;
 	}
 };
+const store = createStore(reducer);
 
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-	number.innerText = countStore.getState();
-};
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-	countStore.dispatch({ type: ADD });
+const dispatchAddToDo = (text) => {
+	store.dispatch(addToDo(text));
 };
 
-const handleMinus = () => {
-	countStore.dispatch({ type: MINUS });
+const dispatchDeleteToDo = (e) => {
+	const id = parseInt(e.target.parentNode.id);
+	store.dispatch(deleteToDo(id));
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const paintToDos = () => {
+	const toDos = store.getState();
+	ul.innerHTML = "";
+	toDos.forEach((toDo) => {
+		const li = document.createElement("li");
+		const btn = document.createElement("button");
+		btn.innerText = "Delete";
+		btn.addEventListener("click", dispatchDeleteToDo);
+		li.id = toDo.id;
+		li.innerText = toDo.text + " ";
+		li.append(btn);
+		ul.appendChild(li);
+	});
+};
+
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+	e.preventDefault();
+	const toDo = input.value;
+	input.value = "";
+	dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
+/***************** End TO DO Vanilla*/
