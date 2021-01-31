0b347ce2ddb32e49727b6cecbd81bd4d551ef998
yesimnoh <yesimnoh@gmail.com>
2021-01-31 17:25:03 +0900

    add createReducer
    createReducer를 통해 switch문 없이 reduce 작성

--- src/todoRedux/todo.reducer.js
+++ src/todoRedux/todo.reducer.js
@@ -1,3 +1,4 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import actionCreators from "./todo.action";
@@ -11,17 +12,26 @@ const initialState = {
 	list: [],
 };
 
-const reducer = (state = initialState, action) => {
-	const { list } = state;
-	switch (action.type) {
-		case actionCreators.addToDo.type:
-			return { list: [{ text: action.payload, id: Date.now() }, ...list] };
-		case actionCreators.deleteToDo.type:
-			return { list: list.filter((toDo) => toDo.id !== action.payload) };
-		default:
-			return state;
-	}
-};
+// const reducer = (state = initialState, action) => {
+// 	const { list } = state;
+// 	switch (action.type) {
+// 		case actionCreators.addToDo.type:
+// 			return { list: [{ text: action.payload, id: Date.now() }, ...list] };
+// 		case actionCreators.deleteToDo.type:
+// 			return { list: list.filter((toDo) => toDo.id !== action.payload) };
+// 		default:
+// 			return state;
+// 	}
+// };
+
+const reducer = createReducer(initialState, {
+	[actionCreators.addToDo]: (state, action) => {
+		state.list.unshift({ text: action.payload, id: Date.now() });
+	},
+	[actionCreators.deleteToDo]: (state, action) => {
+		return { list: state.list.filter((toDo) => toDo.id !== action.payload) };
+	},
+});
 
 // export default reducer;
 export default persistReducer(persistConfig, reducer);
