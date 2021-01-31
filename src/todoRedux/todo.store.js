995b166179d76cb1b1410ea5ca40c4885019cc47
yesimnoh <yesimnoh@gmail.com>
2021-01-31 17:35:20 +0900

    add configureStore setting
    store를 createStore에서 configureStore형태로 수정

--- src/todoRedux/todo.store.js
+++ src/todoRedux/todo.store.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
-import { persistStore } from "redux-persist";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { createStore } from "redux";
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, REGISTER, PURGE } from "redux-persist";
 import reducer from "./todo.reducer";
 
-export const store = createStore(reducer);
+// export const store = createStore(reducer);
+export const store = configureStore({
+	reducer,
+	middleware: getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+		},
+	}),
+});
 export const persistor = persistStore(store);
